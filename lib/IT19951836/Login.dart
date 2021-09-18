bd73aaaa0453dee4feb9982fb4be68c4099a8cda
D.I.P Lakshan <it19951836@my.sliit,lk>
2021-09-18 15:42:13 +0530

    add Text filed widgets

--- lib/IT19951836/Login.dart
+++ lib/IT19951836/Login.dart
@@ -2,7 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:testboc/IT19951836/palatte.dart';
 import 'widgets/background-image.dart';
-import '';
+import 'package:line_icons/line_icons.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() =>runApp(MaterialApp(
   home: Login() ,
@@ -77,62 +78,21 @@ class Login extends StatelessWidget {
                   ),
                   child: Column(
                     children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(
-                            width: 3.0,
-                            color: Colors.amberAccent
-                          ),
-                          borderRadius: BorderRadius.circular(14),
-                            boxShadow: [BoxShadow(blurRadius: 5,color: Colors.grey,offset: Offset(0,4))]
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.symmetric(vertical: 15),
-                            border: InputBorder.none,
-                            hintText: 'Email',
-                            prefixIcon: Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 20),
-                              child: Icon(
-                                Icons.account_circle_rounded,
-                                color: Colors.grey,
-                              ),
-                            ),
-                            hintStyle: TBodyText
-                          ),
-                        ),
+                      TextInput(
+                        icon: FontAwesomeIcons.user,
+                        hint: 'Email',
+                        inputType: TextInputType.emailAddress,
+                        inputAction: TextInputAction.next,
                       ),
 
                       SizedBox(
                         height: 18,
                       ),
 
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            border: Border.all(
-                                width: 3.0,
-                                color: Colors.amberAccent
-                            ),
-                            borderRadius: BorderRadius.circular(14),
-                            boxShadow: [BoxShadow(blurRadius: 5,color: Colors.grey,offset: Offset(0,4))]
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              contentPadding: const EdgeInsets.symmetric(vertical: 15),
-                              border: InputBorder.none,
-                              hintText: 'Password',
-                              prefixIcon: Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 20),
-                                child: Icon(
-                                  Icons.password,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                              hintStyle: TBodyText
-                          ),
-                        ),
+                      PasswordInput(
+                        icon: FontAwesomeIcons.lock,
+                        hint: 'Password',
+                        inputAction: TextInputAction.next,
                       ),
                     ],
                   ),
@@ -142,13 +102,105 @@ class Login extends StatelessWidget {
                   height: 18,
                 ),
 
-
-
               ],
             ),
           ),
         ),
       ],
+    );
+  }
+}
+
+class TextInput extends StatelessWidget {
+  const TextInput({
+    Key? key,
+    required this.icon,
+    required this.hint,
+    required this.inputType,
+    required this.inputAction,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String hint;
+  final TextInputType inputType;
+  final TextInputAction inputAction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          width: 3.0,
+          color: Colors.amberAccent
+        ),
+        borderRadius: BorderRadius.circular(14),
+          boxShadow: [BoxShadow(blurRadius: 5,color: Colors.grey,offset: Offset(0,4))]
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(vertical: 15),
+          border: InputBorder.none,
+          hintText: hint,
+          prefixIcon: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Icon(
+              icon,
+              size: 19,
+              color: Colors.grey,
+            ),
+          ),
+          hintStyle: TBodyText
+        ),
+        keyboardType: inputType,
+        textInputAction: inputAction,
+      ),
+    );
+  }
+}
+
+class PasswordInput extends StatelessWidget {
+  const PasswordInput({
+    Key? key,
+    required this.icon,
+    required this.hint,
+    required this.inputAction,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String hint;
+  final TextInputAction inputAction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(
+              width: 3.0,
+              color: Colors.amberAccent
+          ),
+          borderRadius: BorderRadius.circular(14),
+          boxShadow: [BoxShadow(blurRadius: 5,color: Colors.grey,offset: Offset(0,4))]
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+            contentPadding: const EdgeInsets.symmetric(vertical: 15),
+            border: InputBorder.none,
+            hintText: hint,
+            prefixIcon: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Icon(
+                icon,
+                size: 19,
+                color: Colors.grey,
+              ),
+            ),
+            hintStyle: TBodyText
+        ),
+        obscureText: true,
+        textInputAction: inputAction,
+      ),
     );
   }
 }
