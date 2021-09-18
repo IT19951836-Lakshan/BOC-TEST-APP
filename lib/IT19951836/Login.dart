7d3a548731a7cedf9c8c94a37356e2e6f975f1a7
D.I.P Lakshan <it19951836@my.sliit,lk>
2021-09-18 16:10:36 +0530

    Created Password and text input widgets

--- lib/IT19951836/Login.dart
+++ lib/IT19951836/Login.dart
@@ -2,7 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:testboc/IT19951836/palatte.dart';
 import 'widgets/background-image.dart';
-import 'package:line_icons/line_icons.dart';
+import 'widgets/text-input.dart';
+import 'widgets/password-input.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() =>runApp(MaterialApp(
@@ -110,99 +111,4 @@ class Login extends StatelessWidget {
     );
   }
 }
-
-class TextInput extends StatelessWidget {
-  const TextInput({
-    Key? key,
-    required this.icon,
-    required this.hint,
-    required this.inputType,
-    required this.inputAction,
-  }) : super(key: key);
-
-  final IconData icon;
-  final String hint;
-  final TextInputType inputType;
-  final TextInputAction inputAction;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(
-          width: 3.0,
-          color: Colors.amberAccent
-        ),
-        borderRadius: BorderRadius.circular(14),
-          boxShadow: [BoxShadow(blurRadius: 5,color: Colors.grey,offset: Offset(0,4))]
-      ),
-      child: TextField(
-        decoration: InputDecoration(
-          contentPadding: const EdgeInsets.symmetric(vertical: 15),
-          border: InputBorder.none,
-          hintText: hint,
-          prefixIcon: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Icon(
-              icon,
-              size: 19,
-              color: Colors.grey,
-            ),
-          ),
-          hintStyle: TBodyText
-        ),
-        keyboardType: inputType,
-        textInputAction: inputAction,
-      ),
-    );
-  }
-}
-
-class PasswordInput extends StatelessWidget {
-  const PasswordInput({
-    Key? key,
-    required this.icon,
-    required this.hint,
-    required this.inputAction,
-  }) : super(key: key);
-
-  final IconData icon;
-  final String hint;
-  final TextInputAction inputAction;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(
-              width: 3.0,
-              color: Colors.amberAccent
-          ),
-          borderRadius: BorderRadius.circular(14),
-          boxShadow: [BoxShadow(blurRadius: 5,color: Colors.grey,offset: Offset(0,4))]
-      ),
-      child: TextField(
-        decoration: InputDecoration(
-            contentPadding: const EdgeInsets.symmetric(vertical: 15),
-            border: InputBorder.none,
-            hintText: hint,
-            prefixIcon: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Icon(
-                icon,
-                size: 19,
-                color: Colors.grey,
-              ),
-            ),
-            hintStyle: TBodyText
-        ),
-        obscureText: true,
-        textInputAction: inputAction,
-      ),
-    );
-  }
-}
-
 
